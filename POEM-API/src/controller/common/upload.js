51b64c63db8e3afc7f55746d065694318c2ff649
jellyfish <qleganza@163.com>
2023-02-08 23:56:46 +0800

    分片上传

---
+++ POEM-API/src/controller/common/upload.js
@@ -0,0 +1,322 @@
+
+const fs = require('fs');
+const multipartry = require('multiparty'); //Multiparty是用来解析FormData数据的一款插件，也可以生成唯一名字
+const SparkMD5 = require('spark-md5'); //根据文件内容 用来生成唯一文件名
+const path = require('path');
+const COS = require('cos-nodejs-sdk-v5')
+const config = require("../../config")
+const jwt = require("jsonwebtoken");
+const DB = require("../../db");
+const {fileSuffixTypeUtil, rTime, timestamp} = require("../../utils/timeformat");
+const cos = new COS(config.qcloud.cos);
+const TengXunCos = config.qcloud.tengxunCos
+
+// 延迟函数
+const delay = function (interval) {
+    typeof interval !== 'number' ? interval === 1000 : interval;
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, interval);
+    });
+};
+
+// 基于multiparty插件实现文件上传处理 & form-data解析
+const uploadDir = `${__dirname}/upload`;
+// const baseDir = path.resolve(__dirname, '../');
+const baseDir = path.resolve(__dirname, './');
+
+//multipartry解析formdata数据，是否自动处理上传 auto
+const multipartry_load = function (req, auto) {
+    typeof auto !== 'boolean' ? (auto = false) : null;
+    //处理图片的大小上线
+    let config = {
+        maxFieldsSize: 200 * 1024 * 1024,
+    };
+    //如果是自动上传
+    if (auto) config.uploadDir = uploadDir;
+    return new Promise(async (resolve, reject) => {
+        await delay(); //
+        // 用来将客户端formData 结果解析
+        new multipartry.Form(config).parse(req, (err, fields, files) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                fields,
+                files,
+            });
+        });
+    });
+};
+
+// 检测文件是否已经存在
+const exists = function (path) {
+    return new Promise((resolve) => {
+        fs.access(path, fs.constants.F_OK, (err) => {
+            if (err) resolve(false);
+            return resolve(true);
+        });
+    });
+};
+
+// 创建文件并写入到指定的目录 & 返回客户端结果
+const writeFile = function (res, path, file, filename, stream) {
+    // console.log('path: ', path);
+    // console.log('file: ', file);
+    return new Promise((resolve, reject) => {
+        if (stream) {
+            //创建可读流，可写流，生成文件
+            try{
+                let readStream = fs.createReadStream(file.path);
+                let writeStream = fs.createWriteStream(path);
+                readStream.pipe(writeStream);
+                readStream.on('end',()=>{
+                    resolve({
+                        code: 0,
+                        codeText: '上传成功',
+                    });
+                    //同步地删除文件或符号链接
+                    // fs.unlinkSync(file.path);
+                    res.send({
+                        code: 0,
+                        codeText: '上传成功',
+                    });
+                });
+            }catch(err){
+                resolve({
+                    code: 1,
+                    codeText: err,
+                })
+                res.send({
+                    code: 1,
+                    codeText: err,
+                });
+            }
+            return
+        }
+        fs.writeFile(path, file, (err) => {
+            console.log('写入文件')
+            if (err) {
+                reject(err);
+                res.send({
+                    code: 1,
+                    codeText: err,
+                });
+                return;
+            }
+
+            resolve();
+            res.send({
+                code: 0,
+                codeText: '上传成功',
+                filename: filename,
+                url: path.replace(baseDir, 123),
+            });
+        });
+    }).catch(()=>{});
+};
+
+
+// 大文件上传 & 合并切片
+const merge = (HASH, count) => {
+    return new Promise( async (resolve, reject) => {
+        let path = `${uploadDir}/${HASH}`
+        let fileList = []
+        let suffix
+        let isExists = await exists(path); // 判断文件是否存在
+        if(!isExists) {
+            reject('HASH path  is not found!')
+            return
+        }
+        fileList = fs.readdirSync(path);
+        if (fileList.length < count) {
+            reject('the slice has not been uploaded!')
+            return
+        }
+        fileList.sort((a, b) => {
+            let reg = /_(\d+)/;
+            return reg.exec(a)[1] - reg.exec(b)[1];
+        }).forEach(item => {
+            !suffix ? suffix = /\.([0-9a-zA-Z]+)$/.exec(item)[1] : null // 处理文件后缀
+            //合成文件
+            fs.appendFileSync(`${uploadDir}/${HASH}.${suffix}`, fs.readFileSync(`${path}/${item}`));
+            fs.unlinkSync(`${path}/${item}`);
+        })
+        fs.rmdirSync(path) // 删除临时文件夹
+        console.log( `${uploadDir}/${HASH}.${suffix}----${HASH}.${suffix}`)
+
+        resolve({
+            path: `${uploadDir}/${HASH}.${suffix}`,
+            filename: `${HASH}.${suffix}`
+        })
+    })
+}
+/**
+ * 单一文件上传 [FORM-DATA]
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+exports.upload_single  = async (req, res) => {
+    try {
+        let { files, fields } = await multipartry_load(req, true);
+        console.log(files,'files')
+        let file = (files.file && files.file[0]) || {};
+        res.send({
+            code: 0,
+            codeText: '上传成功',
+            originFilename: file.originFilename,
+            url: file.path.replace(baseDir, `${process.env.SocketPORT}`),
+        });
+    } catch (err) {
+        res.send({
+            code: 1,
+            codeText: err,
+        });
+    }
+};
+
+/**
+ * 单一文件上传 [BASE64]
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+exports.upload_single_base64= async (req, res) => {
+    let file = req.body.file;
+    let filename = req.body.filename;
+    let spark = new SparkMD5.ArrayBuffer(); // 根据文件内容,生成一个hash名字
+    let suffix = /\.([0-9a-zA-Z]+)$/.exec(filename)[1];  //后缀名
+    let isExists = false;
+    let path;
+    file = decodeURIComponent(file);
+    file = file.replace(/^data:image\/\w+;base64,/, '');  //获取真正的文件内容
+    file = Buffer.from(file, 'base64'); // 将base64转成正常的文件格式
+    // console.log('file:123 ', file);
+    spark.append(file);
+    path = `${uploadDir}/${spark.end()}.${suffix}`;
+    await delay();
+    // 检测是否存在
+    isExists = await exists(path);
+    if (isExists) {
+        res.send({
+            code: 0,
+            codeText: 'file is exists',
+            urlname: filename,
+            url: path.replace(baseDir, `${process.env.SocketPORT}`),
+        });
+        return;
+    }
+    // fs.writeFile(res)
+    writeFile(res, path, file, filename, false);
+};
+
+//处理缩略图接口
+exports.upload_single_name=async (req, res) => {
+    try {
+        let { fields, files } = await multipartry_load(req);
+        let file = (files.file && files.file[0]) || {};
+        // console.log('file:321 ', file);
+        let filename = (fields.filename && fields.filename[0]) || '';
+        let path = `${uploadDir}/${filename}`;
+        let isExists = false;
+        isExists = await exists(path);
+        if (isExists) {
+            res.send({
+                code: 0,
+                codeText: 'file is exists',
+                url: path.replace(baseDir, `${process.env.SocketPORT}`),
+            });
+            return;
+        }
+        //写入文件
+
+        await writeFile(res, path, file, filename, true);
+    } catch (e) {
+        res.send({
+            code: 1,
+            codeText: e,
+        });
+    }
+};
+
+/**
+ * 上传切片
+ */
+exports.upload_chunk =  async (req, res) => {
+    try {
+        const { fields, files } = await multipartry_load(req);
+        const file = (files.file && files.file[0]) || {};
+        const filename = (fields.filename && fields.filename[0]) || '';
+        // const path = `${uploadDir}/${filename}`
+        let isExists = false;
+        // 创建存放切片的临时目录
+        const [, HASH] = /^([^_]+)_(\d+)/.exec(filename);
+        let path = `${uploadDir}/${HASH}`; // 用hash生成一个临时文件夹
+        !fs.existsSync(path) ? fs.mkdirSync(path) : null; // 判断该文件夹是否存在，不存在的话，新建一个文件夹
+        path = `${uploadDir}/${HASH}/${filename}`; // 将切片存到临时目录中
+        isExists = await exists(path);
+        if (isExists) {
+            res.send({
+                code: 0,
+                codeText: 'file is already exists',
+                url: path.replace(`${process.env.SocketPORT}`, 123),
+            });
+            return;
+        }
+        writeFile(res, path, file, filename, true);
+    } catch (e) {
+        res.send({
+            code: 1,
+            codeText: e,
+        });
+    }
+};
+
+/**
+ * 合并切片
+ */
+exports.upload_merge= async (req, res) => {
+    const { HASH, count } = req.body;
+    try {
+        const { filename, path } = await merge(HASH, count);
+        console.log(filename,123)
+        res.send({
+            code: 0,
+            codeText: 'merge sucessfully',
+            url: path.replace(baseDir, `${process.env.SocketPORT}`),
+        });
+    } catch (e) {
+        res.send({
+            code: 1,
+            codeText: e,
+        });
+    }
+};
+
+
+exports.upload_already= async (req, res) => {
+    let { HASH } = req.body
+    let path = `${uploadDir}/${HASH}`
+    let fileList = []
+    try {
+        fileList = fs.readdirSync(path)
+        fileList = fileList.sort((a, b) => {
+            let reg = /_(\d+)/;
+            return reg.exec(a)[1] - reg.exec(b)[1]
+        })
+        res.send({
+            code: 0,
+            codeText: '',
+            fileList
+        })
+    } catch (e) {
+        res.send({
+            code: 1,
+            codeText: e,
+            fileList: []
+        })
+    }
+}
--- POEM-API/src/router/common/index.js
+++ POEM-API/src/router/common/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {UploadImage} = require('../../controller/common/uploadImage')
 const {uploadVideo} = require('../../controller/common/uploadVideo')
+const {upload_single,upload_already,upload_chunk,upload_merge,upload_single_base64,upload_single_name} = require('../../controller/common/upload')
 const multer = require('multer');
 // 上传图片
 const uploadImg = multer({dest: 'images/'});
@@ -10,5 +11,10 @@ router.post('/upload', uploadImg.single('file'), UploadImage)
 
 router.post('/uploadVideo', uploadVideoFile.single('file'), uploadVideo)
 
-
+router.post('/upload_single', upload_single)
+router.post('/upload_already', upload_already)
+router.post('/upload_chunk', upload_chunk)
+router.post('/upload_merge', upload_merge)
+router.post('/upload_single_base64', upload_single_base64)
+router.post('/upload_single_name', upload_single_name)
 module.exports = router
